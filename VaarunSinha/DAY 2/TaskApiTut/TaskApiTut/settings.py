1851fffe50ee33c990f08a927b047e04833fc26f
Dev2212 <developervaarun@gmail.com>
2022-01-16 23:16:49 +0530

    hid secret key

--- VaarunSinha/DAY 2/TaskApiTut/TaskApiTut/settings.py
+++ VaarunSinha/DAY 2/TaskApiTut/TaskApiTut/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from dotenv import load_dotenv
+load_dotenv()
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ynurtk+#c*_46_4ampmx&t5+&@r1^5kwl$y1hl2l*4rp0gx*3'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
