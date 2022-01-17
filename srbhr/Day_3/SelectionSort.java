75787aa1e82fcaa9e8e2f033c039ab6baf583248
srbhr <srbh077@gmail.com>
2022-01-17 22:35:44 +0530

    Added Day 3 Progress srbhr/Day_3/SelectionSort.java

---
+++ srbhr/Day_3/SelectionSort.java
@@ -0,0 +1,27 @@
+class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+        selectionSort(arr);
+        for (int i : arr) {
+            System.err.print(i + " ");
+        }
+        System.out.println();
+    }
+
+}
