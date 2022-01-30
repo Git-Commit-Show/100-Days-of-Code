1db6678ed1655030f09751da1ed154ba141ff22c
PabloWolf96 <ikhena.owen47@gmail.com>
2022-01-30 22:10:48 +0100

    day 16

---
+++ PabloWolf96/Leetcode data structures study plan/Day 14/235.js
@@ -0,0 +1,10 @@
+// leetcode proble 235 lowest common ancestor of a BST
+var lowestCommonAncestor = function (root, p, q) {
+  if (p.val < root.val && q.val < root.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else if (p.val > root.val && q.val > root.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
+  }
+};
