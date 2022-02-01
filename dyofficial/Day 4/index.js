e62df30d0404bf7904d14e672eadfba5c6589b20
Daniel <danielkofi4@gmail.com>
2022-02-01 11:48:05 +0000

    Day 4

---
+++ dyofficial/Day 4/index.js
@@ -0,0 +1,17 @@
+function solve(meal_cost, tip_percent, tax_percent) {
+  // Write your code here
+
+  const final = meal_cost + (meal_cost / 100) * tip_percent + (meal_cost / 100) * tax_percent;
+  const finalAmount = Math.floor(final);
+  console.log(finalAmount);
+}
+
+function main() {
+  const meal_cost = parseFloat(readLine().trim());
+
+  const tip_percent = parseInt(readLine().trim(), 10);
+
+  const tax_percent = parseInt(readLine().trim(), 10);
+
+  solve(meal_cost, tip_percent, tax_percent);
+}
