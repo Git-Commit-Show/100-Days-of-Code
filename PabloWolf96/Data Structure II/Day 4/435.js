8e6c158b4b67182004fb9048f622eebb47d65252
PabloWolf96 <ikhena.owen47@gmail.com>
2022-02-04 00:36:57 +0100

    added non-overlapping intervals

--- PabloWolf96/Data Structure II/Day 4/435.js
+++ PabloWolf96/Data Structure II/Day 4/435.js
@@ -1,9 +1,9 @@
 // leetcode problem 435 Non-overlapping intervals
 var eraseOverlapIntervals = function (intervals) {
   intervals.sort((a, b) => a[0] - b[0]);
-  let n = intervals.length;
+  let num = intervals.length;
   let res = 0;
-  let i = n - 1;
+  let i = num - 1;
   while (i > 0) {
     let j = i - 1;
     while (j >= 0 && intervals[j][1] > intervals[i][0]) {
