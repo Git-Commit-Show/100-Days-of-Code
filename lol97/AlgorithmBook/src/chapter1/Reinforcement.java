9263e66026973bbdd7e47f668e264a0b969175e2
lol97 <blackmanga80@gmail.com>
2022-02-19 01:04:26 +0700

    lol97_day20_reinforcement1.2-1.3

--- lol97/AlgorithmBook/src/chapter1/Reinforcement.java
+++ lol97/AlgorithmBook/src/chapter1/Reinforcement.java
@@ -8,21 +8,21 @@ public class Reinforcement {
 	private static void printMenu() {
 		System.out.println("==========================");
 		System.out.println("1. R.1.1 InputAllBaseType");
+		System.out.println("2. R.1.2 GameEntry");
+		System.out.println("3. R.1.3 isMultiple");
 		System.out.println("==========================");
 	}
 	
 	public static void main(String[] args) {
-//		Scanner stdIn = new Scanner(System.in);
-//		printMenu();
-//		System.out.println("Please choose menu : ");
-//		while(!stdIn.hasNextInt()) {
-//			stdIn.hasNextLine();
-//			System.out.println("Invalid integer; please enter an integer: ");
-//		}
-//		int menu = stdIn.nextInt();
-//		stdIn.close();
-//		runMenu(menu);
-		inputAllBaseTypes();
+		Scanner stdIn = new Scanner(System.in);
+		printMenu();
+		System.out.println("Please choose menu : ");
+		while(!stdIn.hasNextInt()) {
+			stdIn.hasNextLine();
+			System.out.println("Invalid integer; please enter an integer: ");
+		}
+		int menu = stdIn.nextInt();
+		runMenu(menu);
 	}
 	
 	private static void runMenu(int menu) {
@@ -30,7 +30,17 @@ public class Reinforcement {
 		case 1:
 			inputAllBaseTypes();
 			break;
-
+		case 2:
+			listGameEntry();
+			break;
+		case 3:
+			Scanner input = new Scanner(System.in);
+			System.out.println("value m : ");
+			long m = input.nextLong();
+			System.out.println("value n : ");
+			long n = input.nextLong();
+			System.out.println("is n Multiple of m : " + isMultiple(m, n));
+			break;
 		default:
 			System.out.println("menu not found");
 			break;
@@ -60,7 +70,30 @@ public class Reinforcement {
 		list.add(inpDouble);
 		
 		System.out.println(list);
-		input.close();
+	}
+	
+	private static void listGameEntry() {
+		GameEntry[] A = new GameEntry[5];
+		A[0] = new GameEntry();
+		A[1] = new GameEntry();
+		A[2] = new GameEntry();
+		A[3] = new GameEntry();
+		A[4] = new GameEntry();
+		GameEntry[] B = A;
+		A[4].score = 440;
+		System.out.println(B[4].score); 
+	}
+	
+	private static boolean isMultiple(long m, long n) {
+		if (m > n) {
+			return false;
+		} else {
+			return n % m == 0;
+		}
 	}
 
+}
+
+class GameEntry{
+	int score;
 }
