3af239015d76a89ce286b666f4c878ab74d9c140
lol97 <blackmanga80@gmail.com>
2022-02-28 21:30:16 +0700

    add jfreechart

--- lol97/AlgorithmBook/src/chapter2/project/DokumenCharPlotter.java
+++ lol97/AlgorithmBook/src/chapter2/project/DokumenCharPlotter.java
@@ -1,11 +1,29 @@
 package chapter2.project;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 
@@ -44,7 +62,7 @@ public class DokumenCharPlotter {
 	 */
 	public DokumenCharPlotter() {
 		initialize();
-
+		
 		openFileChooser = new JFileChooser();
 		openFileChooser.setCurrentDirectory(new File("C:\\Users\\SufyanSaori\\Documents"));
 		openFileChooser.setFileFilter(new FileNameExtensionFilter("TEXT FILES", "txt", "text"));
@@ -58,6 +76,15 @@ public class DokumenCharPlotter {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+		
+		XYDataset dataset = createDataset();
+		JFreeChart chart = createChart(dataset);
+		ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        ChartFrame chartFrame = new ChartFrame("heheh", chart);
+        chartFrame.setVisible(true);
+        chartFrame.setSize(300, 400);
 
 		JLabel fileLabel = new JLabel("File Name");
 		fileLabel.setBounds(10, 11, 62, 14);
@@ -111,4 +138,66 @@ public class DokumenCharPlotter {
 		}
 		return dataMap;
 	}
+	
+	private XYDataset createDataset() {
+
+        XYSeries series1 = new XYSeries("2014");
+        series1.add(18, 530);
+        series1.add(20, 580);
+        series1.add(25, 740);
+        series1.add(30, 901);
+        series1.add(40, 1300);
+        series1.add(50, 2219);
+
+        XYSeries series2 = new XYSeries("2016");
+        series2.add(18, 567);
+        series2.add(20, 612);
+        series2.add(25, 800);
+        series2.add(30, 980);
+        series2.add(40, 1210);
+        series2.add(50, 2350);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series1);
+        dataset.addSeries(series2);
+
+        return dataset;
+    }
+
+    private JFreeChart createChart(final XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Average salary per age",
+                "Age",
+                "Salary (€)",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        XYPlot plot = chart.getXYPlot();
+
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+        renderer.setSeriesPaint(1, Color.BLUE);
+        renderer.setSeriesStroke(1, new BasicStroke(2.0f));
+
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+        plot.setRangeGridlinesVisible(false);
+        plot.setDomainGridlinesVisible(false);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+
+        chart.setTitle(new TextTitle("Average Salary per Age",
+                        new Font("Serif", Font.BOLD, 18)
+                )
+        );
+
+        return chart;
+    }
 }
