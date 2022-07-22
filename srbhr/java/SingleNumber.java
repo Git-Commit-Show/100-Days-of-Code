e2d76d577dec700e6318a247fb8e5d054259d90c
srbhr <srbh077@gmail.com>
2022-07-22 21:19:01 +0530

    Day -1 Progress

---
+++ srbhr/java/SingleNumber.java
@@ -0,0 +1,9 @@
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            ans^=nums[i];
+        }
+        return ans;
+    }
+}
